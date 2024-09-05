5453edfea5fba2424df3bfe97a9304183ba91230
CatInAWizardHat <BeigeModel3@proton.me>
2024-09-05 14:15:13 -0700

    added null-ls debug flag

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -2,8 +2,8 @@ return {
 	"nvimtools/none-ls.nvim",
 	config = function()
 		local null_ls = require("null-ls")
-
 		null_ls.setup({
+      debug = true,
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.clang_format,
