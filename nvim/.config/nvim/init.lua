1616a8d84c14e4c6975f3b698bef66e1c23c4d2a
CatInAWizardHat <125680751+CatInAWizardHat@users.noreply.github.com>
2025-08-09 21:51:52 -0700

    added lsp directory and updated nvim config to use nvim0.12+ native package manager

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,44 +1,63 @@
+vim.o.winborder = "rounded"
+vim.o.tabstop = 2
+vim.o.cursorcolumn = false
+vim.o.ignorecase = true
+vim.o.shiftwidth = 2
+vim.o.smartindent = true
+vim.o.wrap = false
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.shiftwidth = 4
-vim.o.signcolumn = "yes"
-vim.o.wrap = false
-vim.o.tabstop = 4
 vim.o.swapfile = false
-vim.g.mapleader = " "
-vim.o.winborder = "rounded"
+vim.o.termguicolors = true
+vim.o.undofile = true
+vim.o.incsearch = true
+vim.o.signcolumn = "yes"
 
-vim.keymap.set('n', '<leader>o', ':update<CR>:source<CR>')
-vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
+local map = vim.keymap.set
+
+vim.g.mapleader = " "
+
+map('n', '<leader>o', ':update<CR>:source<CR>')
+map('n', '<leader>w', ':write<CR>')
+map('n', '<leader>q', ':quit<CR>')
+
+map({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
+map({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
 
 vim.pack.add({
 	{ src = "https://github.com/vague2k/vague.nvim" },
-	{ src = "https://github.com/ellisonleao/gruvbox.nvim" },
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/echasnovski/mini.pick" },
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/L3MON4D3/LuaSnip" },
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(ev)
-		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if client:supports_method('textDocument/completion') then
-			vim.lsp.completion.enable(true, client.id, ev.buf, { autoTrigger = true })
-		end
-	end,
-})
-vim.cmd("set completeopt+=noselect")
-
+require "mason".setup()
 require "mini.pick".setup()
+require "nvim-treesitter.configs".setup({
+	ensure_installed = { "svelte", "typescript", "javascript", "css", "lua", "go" },
+	highlight = { enable = true },
+})
 require "oil".setup()
 
-vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
-vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
-vim.keymap.set('n', '<leader>e', ':Oil<CR>')
+map('n', '<leader>f', ':Pick files<CR>')
+map('n', '<leader>h', ':Pick help<CR>')
+map('n', '<leader>e', ':Oil<CR>')
+map('n', '<leader>lf', vim.lsp.buf.format)
 
-vim.lsp.enable({ "lua_ls", "gopls" })
-vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
+vim.lsp.enable({ "svelte-language-server", "lua_ls", "gopls" })
 
+-- snippets
+require("luasnip").setup({ enable_autosnippets = true })
+require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" })
+
+local ls = require("luasnip")
+map({ "i" }, "<C-e>", function() ls.expand() end, { silent = true })
+map({ "i", "s" }, "<C-J>", function() ls.jump(1) end, { silent = true })
+map({ "i", "s" }, "<C-K>", function() ls.jump(-1) end, { silent = true })
+
+-- colors
+require "vague".setup({ transparent = true })
 vim.cmd("colorscheme vague")
 vim.cmd(":hi statusline guibg=NONE")
