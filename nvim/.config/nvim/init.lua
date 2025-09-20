e338dd008543e092aaa500011f746dbb47f7282c
Alex <125680751+CatInAWizardHat@users.noreply.github.com>
2025-09-20 16:58:58 -0700

    nvim: removed lsp dir and added nvim-lspconfig package

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -30,6 +30,7 @@ vim.pack.add({
 	{ src = "https://github.com/echasnovski/mini.pick" },
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = 'https://github.com/neovim/nvim-lspconfig' },
 	{ src = "https://github.com/L3MON4D3/LuaSnip" },
 })
 
@@ -46,7 +47,7 @@ map('n', '<leader>h', ':Pick help<CR>')
 map('n', '<leader>e', ':Oil<CR>')
 map('n', '<leader>lf', vim.lsp.buf.format)
 
-vim.lsp.enable({ "svelte-language-server", "lua_ls", "gopls" })
+vim.lsp.enable({ "svelte-language-server", "lua_ls", "gopls", "typescript-language-server" })
 
 -- snippets
 require("luasnip").setup({ enable_autosnippets = true })
